5a77047bc5b5bca02e49218f10d57438056ecc6e
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-03-20 23:16:19 -0300

    Iniciando as configurações inicias do projeto

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <header>
+        <h5>Olá Maria Clara</h5>
+        <p>pts</p>
+      </header>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <p>pts</p>
+          <p>Meta de trocar</p>
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <button>Quero trocar meus pontos</button>
+        <button>Compartilhar com os amigos</button>
+        <button>Assistir vídeos</button>
+      </div>
+    </div>
+  )
+}
