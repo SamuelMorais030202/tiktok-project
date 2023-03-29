5429bedff19f2ce126f3104167f325a6905bd227
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-03-29 17:43:53 -0300

    Correção na página de recuperação de senha

--- src/pages/Password.jsx
+++ src/pages/Password.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
 import '../styles/Password.css';
+import { useHistory } from 'react-router-dom';
 
 export default function Password() {
   const [dateUser, setDateUser] = useState({
     emailNewUser: '',
     cpfNewUser: '',
   });
+
+  const history = useHistory();
 
   const handelChange = ({ target }) => {
     setDateUser({
@@ -16,9 +19,17 @@ export default function Password() {
   };
 
   const handleClick = () => {
-    Axios.post("http://localhost:3004/esqueciSenha", {
-      email: dateUser.emailNewUser,
-    });
+    const validate = dateUser.emailNewUser.includes('@')
+      && dateUser.emailNewUser.includes('.com') && dateUser.cpfNewUser.length === 11
+    if (validate) {
+      Axios.post("http://localhost:3004/esqueciSenha", {
+        email: dateUser.emailNewUser,
+      })
+      alert("Senha enviada, verifique seu email");
+      history.push('/');
+    } else {
+      alert("Email ou cpf inválido");
+    }
   }
 
   return (
